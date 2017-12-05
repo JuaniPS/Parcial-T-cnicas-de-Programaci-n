75fff3443f6119b3a59d8c4ae7eb6ec62bbc9d54
JuaniPS <juanips.jips@gmail.com>
2017-12-05 20:16:31 -0300

    Se continuó con la resolución del Ejercicio 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,15 +1,24 @@
 posicionesDeDisparosDePrueba = [(1,1),(3,4),(1,3),(4,5)]
+
 def batalla_de_botes(mapa, posiciones_a_disparar):
-    botes = [((index + 1), (row.index("b") + 1)) for index, row in enumerate(mapa) if "b" in row]
-    for i in range(len(posiciones_a_disparar)):
-        botes[i] - posiciones_a_disparar[i]
-    return botes
 
+    if any('b' in sublist for sublist in mapa) and len(mapa[0]) == len(mapa[len(mapa) - 1]):
+        width = len(mapa[0])
+        botes = [((posn // width) + 1, (posn % width) + 1)
+                 for posn, elem in enumerate(col for row in mapa for col in row)
+                 if elem == "b"]
+        resultado = set(botes) - set(posiciones_a_disparar)
+        print(posiciones_a_disparar)
+        print(botes)
+        return resultado
+    else:
+        return []
 
-assert (batalla_de_botes(["b.b.","....","..bb","b.b"],posicionesDeDisparosDePrueba) == [])
-assert (batalla_de_botes(["b.b..","b...b",".....","....b"],posicionesDeDisparosDePrueba) == [(2,1),(2,5)])
-assert (batalla_de_botes(["b..","...","..b"],[]) == [(1,1),(3,3)])
-
-
-
-[(1,1),(3,4),(1,3),(4,5)]
+'''assert (batalla_de_botes([],posicionesDeDisparosDePrueba) == [])
+assert (batalla_de_botes([""],posicionesDeDisparosDePrueba) == [])
+assert (batalla_de_botes(["      "],posicionesDeDisparosDePrueba) == [])
+assert (batalla_de_botes(["soy NO valido"],posicionesDeDisparosDePrueba) == [])
+assert (batalla_de_botes(["yo","tambien","soy","invalido"],posicionesDeDisparosDePrueba) == [])
+assert (batalla_de_botes(["b.b.","....","..bb","b.b"],posicionesDeDisparosDePrueba) == [])'''
+print (batalla_de_botes(["b.b..","b...b",".....","....b"],posicionesDeDisparosDePrueba))
+'''print(batalla_de_botes(["b..","...","..b"],[]))'''
--- pruebas 2.py
+++ pruebas 2.py
@@ -1,12 +1,13 @@
-def batalla_de_botes(mapa, posiciones_a_disparar):
-    botes = [((index + 1), (row.index("b") + 1)) for index, row in enumerate(mapa) if "b" in row]
-    for i in range(max(len(botes), len(posiciones_a_disparar))):
-        botes.remove(posiciones_a_disparar[i])
-    return botes
+mapa = ["b.b..","b...b",".....","....b"]
 
-posicionesDeDisparosDePrueba = [(1,1),(3,4),(1,3),(4,5)]
-
-print(batalla_de_botes(["b.b.","....","..bb","b.b"],posicionesDeDisparosDePrueba) == [])
+print(any('b' in sublist for sublist in mapa))
 
 
 
+
+
+
+
+
+
+len(mapa[0]) == len(mapa[len(mapa) - 1])
